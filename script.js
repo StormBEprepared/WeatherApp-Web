54a3461237cea24b6bbd977932fcc4cba7056995
Stefan <leonidst@amazon.com>
2023-01-20 16:16:48 +0000

    added bits. tbc...

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ var foods = {
   "chocolate":300,
   "broccolli":65
   };
+
+
+function get_action() { // Inside script tags
+
+    var fn = document.getElementById("kcal").value;
+    var ln = document.getElementById("protein").value;
+    var foodType=get_dropdown_value("foodType");
+    alert(`Is this working? ${fn} , ${ln}, ${foodType}. `);//Yes it is.
+}
+
+
+function get_dropdown_value(elemID){
+    var element=document.getElementById(elemID);
+    return element.value;
+}
