08cbfbf334d8f32b9be8620a5894cd3374ccefda
Leonid-Stefanel <stefanpintea33@gmail.com>
2023-03-24 22:25:28 +0000

    finished app. added to pers web.

--- script.js
+++ script.js
@@ -1,42 +1,14 @@
-//values per 100g/ cooked => name : kcal
-var foods = {
-  "chicken breast": 125,
-  "chocolate":300,
-  "broccolli":65
-  };
 
-
-function get_action() { // Inside script tags
-
-    var ft=get_dropdown_value("foodType");
-    var kc = document.getElementById("kcal").value;
-    var prt = document.getElementById("protein").value;
-   
-    alert(`Is this working? ${ft} , ${kc}, ${prt}. `);//Yes it is.
-}
-function updateValues(){
-  var ft=get_dropdown_value("foodType");
-  var kc = document.getElementById("kcal").value;
-  var prt = document.getElementById("protein").value;
-  if (ft=="chicken breast") {//not working, must do function individually for each element to get it's 
-    //innertext/innerhtml? updated once value in specific element is selected
-    kc=150;
-    document.getElementById("kcal").value=kc;
-    //document.getElementById("kcal").innerHTML=kc;
-
-    prt=25;
-    document.getElementById("protein").value=prt;
-  }else{
-    document.getElementById("protein").value=0;
-    document.getElementById("kcal").value=0;
-
-  }
+function EnterEventHandler(){
+  var input = document.getElementById("InputCity");
+  input.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      document.getElementById("checkWeatherForCity").click();
+    }
+  });
 }
 
-function get_dropdown_value(elemID){
-    var element=document.getElementById(elemID);
-    return element.value;
-}
 
 function sendApiGetReq(){
   var requestOptions = {
@@ -49,16 +21,23 @@ function sendApiGetReq(){
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=30ad8b12cff1d96e78b93a835665538e`, requestOptions)
     .then(response => response.json())
     .then(json => {
-      document.getElementById("Temperature").innerHTML=`Temperature: ${parseInt(json.main.temp)-272}<span>°C</span>`;//-272 as the json.main.temp is in kelvin
-      document.getElementById("City").innerHTML=`City: ${json.name}.`;
-      document.getElementById("Main").innerHTML=`State: ${json.weather[0].main}.`;
-      document.getElementById("Description").innerHTML=`Details: ${json.weather[0].description}.`;
-      document.getElementById("Country").innerHTML=`Country: ${json.sys.country}.`;
 
+      document.getElementById("Temperature").innerHTML=`Temperature: ${parseInt(json.main.temp)-272}<span>°C</span>`;//-272 as the json.main.temp is in kelvin
+      document.getElementById("City").innerHTML=`City: ${json.name}`;
+      document.getElementById("Main").innerHTML=`State: ${json.weather[0].main}`;
+      document.getElementById("Description").innerHTML=`Details: ${json.weather[0].description}`;
+      document.getElementById("Country").innerHTML=`Country: ${json.sys.country}`;
+
+      const now = new Date(); // Get the current date and time
+      const hours = now.getHours(); // Get the current hour (0-23)
       const image = document.querySelector('.weather-box img');
       switch(json.weather[0].main){
         case 'Clear':
-          image.src = 'images/clear.png';
+          if (hours >=22) {
+            image.src = 'images/nightClear.png';
+          } else {
+            image.src = 'images/clear.png';
+          }
           break;
         case 'Rain':
           image.src = 'images/rain.png';
@@ -67,8 +46,11 @@ function sendApiGetReq(){
           image.src = 'images/snow.png';
           break;
         case 'Clouds':
-          image.src = 'images/cloud.png';
-          break;
+          if (hours >=22) {
+            image.src = 'images/nightClouds.png';
+          } else {
+            image.src = 'images/cloud.png';
+          }          break;
         case 'Haze':
           image.src = 'images/mist.png';
           break;
@@ -76,8 +58,17 @@ function sendApiGetReq(){
           image.src = '';
       }
     
+      
     } )
     .catch(error => console.log('error', error));
     
+    document.getElementById("InputCity").value=null;
+    
+    
+
+    
 
 }
+
+
+
